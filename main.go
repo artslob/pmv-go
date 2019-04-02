ec83bb881418e80523a743196aafda5dc11fa3c1
artslob <artslob@yandex.ru>
2019-04-02 11:08:51 +0300

    move print tree listener to separate file

--- main.go
+++ main.go
@@ -4,71 +4,7 @@ import (
 	"fmt"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/artslob/pmv/parser"
-	"strings"
 )
-
-type treePrintListener struct {
-	*parser.BaseLangListener
-	b         *strings.Builder
-	ruleNames []string
-	level     int
-}
-
-func newTreePrintListener(ruleNames []string) *treePrintListener {
-	return &treePrintListener{b: &strings.Builder{}, ruleNames: ruleNames}
-}
-
-func (s *treePrintListener) String() string {
-	return s.b.String()
-}
-
-func (s *treePrintListener) print(str string) {
-	s.b.WriteString(str)
-}
-
-func (s *treePrintListener) printLineBreak() {
-	s.b.WriteString("\n")
-}
-
-func (s *treePrintListener) println(str string) {
-	s.print(str)
-	s.printLineBreak()
-}
-
-func (s *treePrintListener) getIndent() string {
-	indent := "  "
-	return strings.Repeat(indent, s.level)
-}
-
-func (s *treePrintListener) printIndent() {
-	s.print(s.getIndent())
-}
-
-func (s *treePrintListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
-	s.printIndent()
-	s.println(s.ruleNames[ctx.GetRuleIndex()])
-	s.level++
-}
-
-func (s *treePrintListener) ExitEveryRule(ctx antlr.ParserRuleContext) {
-	s.level--
-}
-
-func (s *treePrintListener) VisitTerminal(node antlr.TerminalNode) {
-	s.printIndent()
-	s.print(node.GetText())
-	if node.GetSymbol().GetTokenType() < parser.LangParserIDENTIFIER {
-		s.printLineBreak()
-		return
-	}
-	if token := node.GetSymbol(); token != nil {
-		s.print(fmt.Sprintf("  [line %d, offset: %d]\n", token.GetLine(), token.GetColumn()))
-	}
-}
-
-func (s *treePrintListener) ExitTerminal(node antlr.TerminalNode) {
-	s.level--
-}
 
 func parseToStringAst(input string) string {
 	is := antlr.NewInputStream(input)
