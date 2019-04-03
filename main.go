583c8a3afe9683528c99448c085341facda12149
artslob <artslob@yandex.ru>
2019-04-03 19:45:15 +0300

    first lab: main program implemented

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/artslob/pmv-go/parser"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 func getParser(input string) *parser.LangParser {
@@ -18,6 +22,30 @@ func parseToStringAst(input string) string {
 	listener := newTreePrintListener(p.GetRuleNames())
 	antlr.ParseTreeWalkerDefault.Walk(listener, p.Source())
 	return listener.String()
+}
+
+func checkError(e error) {
+	if e != nil {
+		log.Println(e)
+		os.Exit(2)
+	}
+}
+
+func lab1() {
+	CommandLine := flag.NewFlagSet("lab1", flag.ExitOnError)
+	inputFilename := CommandLine.String("i", "input.txt", "Input file with text of a program to parse.")
+	outputFilename := CommandLine.String("o", "", "Output file where parsed tree will be written. Default - stdout.")
+	_ = CommandLine.Parse(os.Args[1:])
+	data, err := ioutil.ReadFile(*inputFilename)
+	checkError(err)
+	input := string(data)
+	output := parseToStringAst(input)
+	if *outputFilename == "" {
+		fmt.Print(output)
+	} else {
+		err := ioutil.WriteFile(*outputFilename, []byte(output), 0644)
+		checkError(err)
+	}
 }
 
 func main() {
