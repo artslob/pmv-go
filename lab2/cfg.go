89313b48fdcd420cf3c549dc81720cc04287e147
artslob <artslob@yandex.ru>
2019-04-07 15:05:40 +0300

    move block code to separate file

--- lab2/cfg.go
+++ lab2/cfg.go
@@ -1,38 +1,8 @@
 package lab2
 
 import (
-	"fmt"
 	"github.com/artslob/pmv-go/parser"
 )
-
-type BlockClass int
-
-const (
-	DEFAULT BlockClass = iota
-	START
-	END
-)
-
-type Block struct {
-	id    int
-	text  string
-	next  *Block
-	level int
-	class BlockClass
-}
-
-func (b *Block) String() string {
-	label := ""
-	switch b.class {
-	case START:
-		label = "START"
-	case END:
-		label = "END"
-	default:
-		label = b.text
-	}
-	return fmt.Sprintf("%2d [label=\"%s\"]\n", b.id, label)
-}
 
 type CFGListener struct {
 	*parser.BaseLangListener
