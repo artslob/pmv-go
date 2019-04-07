b9bd62156abdd8c84c3359fba3f3bb38b1f4aae7
artslob <artslob@yandex.ru>
2019-04-07 17:39:53 +0300

    block stack implementation

--- lab2/block.go
+++ lab2/block.go
@@ -29,3 +29,26 @@ func (b *Block) String() string {
 	}
 	return fmt.Sprintf("%2d [label=\"%s\"]\n", b.id, label)
 }
+
+type BlockStack []*Block
+
+func (s *BlockStack) Push(b *Block) {
+	*s = append(*s, b)
+}
+
+func (s *BlockStack) Pop() *Block {
+	res := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return res
+}
+
+func (s *BlockStack) Peek() *Block {
+	if len(*s) == 0 {
+		return nil
+	}
+	return (*s)[len(*s)-1]
+}
+
+func (s *BlockStack) Size() int {
+	return len(*s)
+}
