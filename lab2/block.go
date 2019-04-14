6d7380213633a9525843cfe83c7445c1980d1daa
artslob <artslob@yandex.ru>
2019-04-14 11:38:08 +0300

    initial commit for lab2v3

--- lab2/block.go
+++ lab2/block.go
@@ -2,35 +2,16 @@ package lab2
 
 import "fmt"
 
-type BlockClass int
-
-const (
-	DEFAULT BlockClass = iota
-	START
-	END
-	IF
-)
-
 type Block struct {
 	id     int
 	text   string
 	next   *Block
 	branch *Block
-	class  BlockClass
 	level  int
 }
 
 func (b *Block) String() string {
-	label := ""
-	switch b.class {
-	case START:
-		label = "START"
-	case END:
-		label = "END"
-	default:
-		label = b.text
-	}
-	return fmt.Sprintf("%2d [label=\"%s\"]\n", b.id, label)
+	return fmt.Sprintf("%2d [label=\"%s\"]\n", b.id, b.text)
 }
 
 type BlockStack []*Block
--- lab2/cfg.go
+++ lab2/cfg.go
@@ -15,8 +15,8 @@ type CFGListener struct {
 
 func NewCFGListener() *CFGListener {
 	l := &CFGListener{}
-	l.start = &Block{class: START, id: -1}
-	l.end = &Block{class: END, id: -2}
+	l.start = &Block{id: -1}
+	l.end = &Block{id: -2}
 	l.blocks.Push(l.start)
 	return l
 }
@@ -24,51 +24,4 @@ func NewCFGListener() *CFGListener {
 func (s *CFGListener) nextId() int {
 	s.currentId++
 	return s.currentId
-}
-
-func (s *CFGListener) ExitExpression(ctx *parser.ExpressionContext) {
-	block := &Block{id: s.nextId(), text: ctx.GetText(), level: s.level}
-	topBlock := s.blocks.Peek()
-	defer s.blocks.Push(block)
-	if topBlock.level != s.level {
-		return
-	}
-	if topBlock.next == nil {
-		topBlock.next = block
-	} else if topBlock.class == IF {
-		topBlock.next.next = block
-		topBlock.branch = block
-	}
-}
-
-func (s *CFGListener) ExitFuncDef(ctx *parser.FuncDefContext) {
-	s.blocks.Pop().next = s.end
-}
-
-func (s *CFGListener) ExitIfExpr(ctx *parser.IfExprContext) {
-	ifExpr := &Block{id: s.nextId(), text: ctx.Expr().GetText(), class: IF, level: s.level}
-	s.blocks.Push(ifExpr)
-}
-
-func (s *CFGListener) EnterIfThen(ctx *parser.IfThenContext) {
-	s.level++
-}
-
-func (s *CFGListener) ExitIfThen(ctx *parser.IfThenContext) {
-	s.level--
-}
-
-func (s *CFGListener) ExitIf(ctx *parser.IfContext) {
-	withElse := ctx.IfElse() != nil
-	// on stack: n: else, n-1: then, n-2: if
-	if withElse == true {
-		// TODO
-		return
-	}
-	// on stack: n: then, n-1: if
-	then := s.blocks.Pop()
-	ifExpr := s.blocks.Pop()
-	ifExpr.next = then
-	s.blocks.Peek().next = ifExpr
-	s.blocks.Push(ifExpr)
 }
