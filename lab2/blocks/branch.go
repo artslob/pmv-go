71dd2e84f67d1dae7f2434287c9fcc7b14381f9e
artslob <artslob@yandex.ru>
2019-06-17 11:48:12 +0300

    set shape=diamond for branch blocks

--- lab2/blocks/branch.go
+++ lab2/blocks/branch.go
@@ -1,8 +1,14 @@
 package blocks
 
+import "fmt"
+
 type BranchBlock struct {
 	DefaultBlock
 	branch Block
+}
+
+func (b *BranchBlock) String() string {
+	return fmt.Sprintf("%2d [label=\"%s\",shape=diamond]\n", b.Id, b.Text)
 }
 
 func (b *BranchBlock) GetAddress() Block {
