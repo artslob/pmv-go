0206b084ee6f4ee92ad7571d2bbae28315b2b557
artslob <artslob@yandex.ru>
2019-06-20 20:59:21 +0300

    carry out common functionality in tests: get file content from dir, compare parsed input to expected output

--- lab2_test.go
+++ lab2_test.go
@@ -7,26 +7,54 @@ import (
 	"testing"
 )
 
-func TestCfgListener(t *testing.T) {
-	cfgInputDir := filepath.Join("testdata", "cfg-input")
-	tests, err := ioutil.ReadDir(cfgInputDir)
+type TestParser interface {
+	// use method under test to process input data
+	Parse(got string) string
+	// dir that contains files with input content for test
+	InputDir() string
+	// dir that contains files with expected output
+	OutputDir() string
+}
+
+type CfgTestParser struct {
+}
+
+func (CfgTestParser) Parse(got string) string {
+	head := lab2.ParseInputToCFG(string(got))
+	printer := lab2.NewCfgPrinter()
+	printer.Print(head)
+	return printer.String()
+}
+
+func (CfgTestParser) InputDir() string {
+	return filepath.Join("testdata", "cfg-input")
+}
+
+func (CfgTestParser) OutputDir() string {
+	return filepath.Join("testdata", "cfg-output")
+}
+
+type CmpFileContentTester struct {
+	TestParser
+}
+
+func (tester CmpFileContentTester) RunTest(t *testing.T) {
+	inputDir := tester.InputDir()
+	tests, err := ioutil.ReadDir(inputDir)
 	if err != nil {
-		t.Fatalf("failed to read files in dir %q: %q", cfgInputDir, err)
+		t.Fatalf("failed to read files in dir %q: %q", inputDir, err)
 	}
 	for _, testFile := range tests {
 		if testFile.IsDir() {
 			continue
 		}
 		testName := testFile.Name()
-		input, err := ioutil.ReadFile(filepath.Join(cfgInputDir, testName))
+		input, err := ioutil.ReadFile(filepath.Join(inputDir, testName))
 		if err != nil {
 			t.Fatalf("failed to read test %q: %s", testName, err)
 		}
-		head := lab2.ParseInputToCFG(string(input))
-		printer := lab2.NewCfgPrinter()
-		printer.Print(head)
-		parsed := printer.String()
-		file := filepath.Join("testdata", "cfg-output", testName)
+		parsed := tester.Parse(string(input))
+		file := filepath.Join(tester.OutputDir(), testName)
 		if *update {
 			t.Logf("update golden file %s", file)
 			if err := ioutil.WriteFile(file, []byte(parsed), 0644); err != nil {
@@ -40,8 +68,15 @@ func TestCfgListener(t *testing.T) {
 		expected := string(content)
 		if parsed != expected {
 			t.Errorf("parsed input not equal to expected in test %q", testName)
-			t.Logf("%q\n", expected)
-			t.Logf("%q\n", parsed)
+			t.Logf("parsed:   %q\n", parsed)
+			t.Logf("expected: %q\n", expected)
 		}
 	}
+}
+
+func TestCfgListener(t *testing.T) {
+	tester := CmpFileContentTester{
+		TestParser: CfgTestParser{},
+	}
+	tester.RunTest(t)
 }
