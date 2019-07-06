7705247458559f4dd43e4f1ece17a4932341e367
artslob <artslob@yandex.ru>
2019-07-06 15:49:56 +0300

    push command -> base command

--- lab3/commands/command.go
+++ lab3/commands/command.go
@@ -7,3 +7,24 @@ type Command interface {
 	Length() int
 	fmt.Stringer
 }
+
+type BaseCommand struct {
+	code      Code
+	length    int
+	argString string
+}
+
+func (cmd BaseCommand) GetOpCode() Code {
+	return cmd.code
+}
+
+func (cmd BaseCommand) Length() int {
+	return cmd.length
+}
+
+func (cmd BaseCommand) String() string {
+	if cmd.argString == "" {
+		return cmd.GetOpCode().String()
+	}
+	return fmt.Sprintf("%s %s", cmd.GetOpCode().String(), cmd.argString)
+}
--- lab3/commands/push.go
+++ lab3/commands/push.go
@@ -1,63 +1,41 @@
 package commands
 
 import (
-	"fmt"
 	"strconv"
 )
 
-type PushCommand struct {
-	code      Code
-	length    int
-	argString string
-}
-
-func (cmd PushCommand) GetOpCode() Code {
-	return cmd.code
-}
-
-func (cmd PushCommand) Length() int {
-	return cmd.length
-}
-
-func (cmd PushCommand) String() string {
-	if cmd.argString == "" {
-		return cmd.GetOpCode().String()
-	}
-	return fmt.Sprintf("%s %s", cmd.GetOpCode().String(), cmd.argString)
-}
-
 type PushByteCommand struct {
-	PushCommand
+	BaseCommand
 	Arg byte
 }
 
 func NewPushByteCommand(arg byte) *PushByteCommand {
-	return &PushByteCommand{PushCommand: PushCommand{code: PushByte, length: 2, argString: strconv.Itoa(int(arg))}, Arg: arg}
+	return &PushByteCommand{BaseCommand: BaseCommand{code: PushByte, length: 2, argString: strconv.Itoa(int(arg))}, Arg: arg}
 }
 
 type PushIntCommand struct {
-	PushCommand
+	BaseCommand
 	Arg int32
 }
 
 func NewPushIntCommand(arg int32) *PushIntCommand {
-	return &PushIntCommand{PushCommand: PushCommand{code: PushInt, length: 5, argString: strconv.Itoa(int(arg))}, Arg: arg}
+	return &PushIntCommand{BaseCommand: BaseCommand{code: PushInt, length: 5, argString: strconv.Itoa(int(arg))}, Arg: arg}
 }
 
 type PushLongCommand struct {
-	PushCommand
+	BaseCommand
 	Arg int64
 }
 
 func NewPushLongCommand(arg int64) *PushLongCommand {
-	return &PushLongCommand{PushCommand: PushCommand{code: PushLong, length: 9, argString: strconv.FormatInt(arg, 10)}, Arg: arg}
+	return &PushLongCommand{BaseCommand: BaseCommand{code: PushLong, length: 9, argString: strconv.FormatInt(arg, 10)}, Arg: arg}
 }
 
 type PushReferenceCommand struct {
-	PushCommand
+	BaseCommand
 	Arg int32
 }
 
 func NewPushReferenceCommand(arg int32) *PushReferenceCommand {
-	return &PushReferenceCommand{PushCommand: PushCommand{code: PushReference, length: 5, argString: strconv.Itoa(int(arg))}, Arg: arg}
+	return &PushReferenceCommand{BaseCommand: BaseCommand{code: PushReference, length: 5, argString: strconv.Itoa(int(arg))}, Arg: arg}
 }
