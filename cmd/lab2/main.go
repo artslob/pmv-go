1cd4494434bd87dd4559a9cd7a4f21212dba72c1
artslob <artslob@yandex.ru>
2019-07-17 18:17:00 +0300

    parse cfg for files like required in lab 2

--- cmd/lab2/main.go
+++ cmd/lab2/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"github.com/artslob/pmv-go/lab2"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-func main() {
+func simple() {
 	var input = `
 def func()
 	do {
@@ -33,4 +40,68 @@ end
 	printer := lab2.NewCfgPrinter()
 	printer.Print(head)
 	fmt.Print(printer.String())
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func flagIsRequired(f *string, name string) {
+	if strings.TrimSpace(*f) == "" {
+		log.Fatalf("%s flag is required", name)
+	}
+}
+
+func directoryExist(path string) (bool, error) {
+	stat, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	if stat.Mode().IsDir() {
+		return true, nil
+	}
+	return false, nil
+}
+
+func lab2Main() {
+	filesFlag := flag.String("files", "", "Set of source filesFlag to be parsed. "+
+		`Specify like this: -files "in1 in2 in3".`)
+	dir := flag.String("dir", "", "Output dir for cfg representation")
+	flag.Parse()
+	flagIsRequired(filesFlag, "files")
+	flagIsRequired(dir, "dir")
+
+	exist, err := directoryExist(*dir)
+	checkError(err)
+	if !exist {
+		checkError(errors.New(fmt.Sprintf("specified directory does not exist: %q", *dir)))
+	}
+
+	files := strings.Split(strings.TrimSpace(*filesFlag), " ")
+	var allContent []byte
+
+	for _, file := range files {
+		file = strings.TrimSpace(file)
+		if file == "" {
+			continue
+		}
+		content, err := ioutil.ReadFile(file)
+		checkError(err)
+		allContent = append(allContent, content...)
+		head := lab2.ParseInputToCFG(string(content))
+		printer := lab2.NewCfgPrinter()
+		printer.Print(head)
+		output := printer.String()
+		err = ioutil.WriteFile(filepath.Join(*dir, fmt.Sprintf("%s.pmv", file)), []byte(output), 0644)
+		checkError(err)
+	}
+
+	// TODO all content file with links between function calls
+}
+
+func main() {
+	lab2Main()
 }
