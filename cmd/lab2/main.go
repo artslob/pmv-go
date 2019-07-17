c4778904b430ba49cbe690f12b116616ce6f9c46
artslob <artslob@yandex.ru>
2019-07-17 21:03:27 +0300

    add main entry for tests

--- cmd/lab2/main.go
+++ cmd/lab2/main.go
@@ -12,23 +12,6 @@ import (
 	"strings"
 )
 
-func simple() {
-	var input = `
-def func()
-	a = a + qwe() + qwe();
-	print(t);
-end
-
-def qwe()
-	a = 1;
-	func();
-	z = func() + test() + func() + t();
-end
-`
-	functionBlocks := lab2.ParseInputToCFG(input)
-	fmt.Print(lab2.NewCfgPrinter().Print(functionBlocks, true).String())
-}
-
 func checkError(err error) {
 	if err != nil {
 		fmt.Println(err)
