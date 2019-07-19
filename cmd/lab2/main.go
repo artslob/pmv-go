7df8981860d1f92389376ae5b2444e3f50649c69
artslob <artslob@yandex.ru>
2019-07-19 16:32:36 +0300

    create cfg for concatenated files with links

--- cmd/lab2/main.go
+++ cmd/lab2/main.go
@@ -36,6 +36,18 @@ func directoryExist(path string) (bool, error) {
 	return false, nil
 }
 
+type ConcatenatedFiles []byte
+
+func (cf *ConcatenatedFiles) append(bytes []byte) {
+	if len(bytes) == 0 {
+		return
+	}
+	if len(*cf) != 0 {
+		*cf = append(*cf, []byte("\n")...)
+	}
+	*cf = append(*cf, bytes...)
+}
+
 func main() {
 	filesFlag := flag.String("files", "", "Set of source filesFlag to be parsed. "+
 		`Specify like this: -files "in1 in2 in3".`)
@@ -51,7 +63,7 @@ func main() {
 	}
 
 	files := strings.Split(strings.TrimSpace(*filesFlag), " ")
-	var allContent []byte
+	var cf ConcatenatedFiles
 
 	for _, file := range files {
 		file = strings.TrimSpace(file)
@@ -60,12 +72,13 @@ func main() {
 		}
 		content, err := ioutil.ReadFile(file)
 		checkError(err)
-		allContent = append(allContent, content...)
+		cf.append(content)
 		functionBlocks := lab2.ParseInputToCFG(string(content))
 		output := lab2.NewCfgPrinter().Print(functionBlocks, false).String()
 		err = ioutil.WriteFile(filepath.Join(*dir, fmt.Sprintf("%s.pmv", file)), []byte(output), 0644)
 		checkError(err)
 	}
 
-	// TODO all content file with links between function calls
+	output := lab2.NewCfgPrinter().Print(lab2.ParseInputToCFG(string(cf)), true).String()
+	err = ioutil.WriteFile(filepath.Join(*dir, "concatenated.pmv"), []byte(output), 0644)
 }
